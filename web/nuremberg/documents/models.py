844c96a5b9b021d3ebbdf618b87a7664bb0d2312
Natalia <nataliabidart@gmail.com>
2022-10-25 16:03:10 -0300

    Restore `# pragma: no cover` for `slug` method

--- web/nuremberg/documents/models.py
+++ web/nuremberg/documents/models.py
@@ -51,7 +51,7 @@ class Document(models.Model):
         if date:
             return date.as_date()
 
-    def slug(self):
+    def slug(self):  # pragma: no cover
         # Try to extract the "genre term" from the descriptive title
         try:
             words = self.title.split(' ')
