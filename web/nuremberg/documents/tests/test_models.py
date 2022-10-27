658edef73fc4d0e266e417b2d16483f608cec462
Natalia <nataliabidart@gmail.com>
2022-10-27 15:49:57 -0300

    Added a few more tests and fixed found bugs

--- web/nuremberg/documents/tests/test_models.py
+++ web/nuremberg/documents/tests/test_models.py
@@ -1,5 +1,4 @@
 import datetime
-from functools import partial
 
 import pytest
 from model_bakery import baker
@@ -35,12 +34,6 @@ def test_document_date_invalid_date(day, month, year):
 def test_document_date_valid_as_date(day, month, year):
     d = DocumentDate.objects.create(day=day, month=month, year=year)
     assert d.as_date() == datetime.date(year, month, day)
-
-
-# no qualifiers for now
-make_property = partial(
-    baker.make, 'PersonalAuthorProperty', qualifier='', qualifier_value=''
-)
 
 
 def test_author_properties_no_match():
@@ -146,7 +139,8 @@ def test_author_properties_uses_property_ranks():
     rank0 = baker.make(
         'PersonalAuthorPropertyRank', name='ignore due to rank 0', rank=0
     )
-    prop0 = make_property(
+    prop0 = baker.make(
+        'PersonalAuthorProperty',
         personal_author=author,
         name=rank0.name,
         value='Ignored 0',
@@ -156,14 +150,16 @@ def test_author_properties_uses_property_ranks():
     rank_negative_1 = baker.make(
         'PersonalAuthorPropertyRank', name='ignore due to rank -1', rank=-1
     )
-    prop_1 = make_property(
+    prop_1 = baker.make(
+        'PersonalAuthorProperty',
         personal_author=author,
         name=rank_negative_1.name,
         value='Ignored -1',
     )
     assert prop_1.rank == -1
 
-    prop_none = make_property(
+    prop_none = baker.make(
+        'PersonalAuthorProperty',
         personal_author=author,
         name='no rank available',
         value='Ignored None',
@@ -171,24 +167,33 @@ def test_author_properties_uses_property_ranks():
     assert prop_none.rank is None
 
     rank_1 = baker.make('PersonalAuthorPropertyRank', name='valid 1', rank=1)
-    prop_1_1 = make_property(
+    prop_1_1 = baker.make(
+        'PersonalAuthorProperty',
         personal_author=author,
         name=rank_1.name,
         value='one value',
+        qualifier='',
+        qualifier_value='',
     )
     assert prop_1_1.rank == 1
-    prop_1_2 = make_property(
+    prop_1_2 = baker.make(
+        'PersonalAuthorProperty',
         personal_author=author,
         name=rank_1.name,
         value='another',
+        qualifier='',
+        qualifier_value='',
     )
     assert prop_1_2.rank == 1
 
     rank_5 = baker.make('PersonalAuthorPropertyRank', name='valid 5', rank=5)
-    prop_5 = make_property(
+    prop_5 = baker.make(
+        'PersonalAuthorProperty',
         personal_author=author,
         name=rank_5.name,
         value='different value, could be a link',
+        qualifier='',
+        qualifier_value='',
     )
     assert prop_5.rank == 5
 
@@ -217,6 +222,106 @@ def test_author_properties_uses_property_ranks():
     }
 
 
+def test_author_properties_groups_qualifiers():
+    author = make_author()
+    name = author.full_name()
+
+    rank = baker.make('PersonalAuthorPropertyRank', name='a property', rank=10)
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank.name,
+        value='some value',
+        qualifier='country',
+        qualifier_value='US',
+    )
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank.name,
+        value='some value',
+        qualifier='country',
+        qualifier_value='CA',
+    )
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank.name,
+        value='another value',  # should not group with previous qualifier
+        qualifier='country',
+        qualifier_value='ES',
+    )
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank.name,
+        value='some value',
+        qualifier='county',  # different spelling but same qualifier
+        qualifier_value='Village',
+    )
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank.name,
+        value='some value',
+        qualifier='subject has role',  # should be replaced by 'role'
+        qualifier_value='role 1',
+    )
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank.name,
+        value='some value',
+        qualifier='object has role',  # should be replaced by 'role'
+        qualifier_value='role 2',
+    )
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank.name,
+        value='some value',
+        qualifier='role',
+        qualifier_value='other role',
+    )
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank.name,
+        value='some value',
+        qualifier='valid in place',  # should be ignored
+        qualifier_value='ignored',
+    )
+
+    result = DocumentPersonalAuthor.objects.properties(name)
+
+    # order is given by higher rank first, then value
+    expected = [
+        {
+            'rank': 10,
+            'name': 'a property',
+            'prop_values': [
+                {
+                    'value': 'another value',
+                    'qualifiers': [('country', ['ES'])],
+                },
+                {
+                    'value': 'some value',
+                    'qualifiers': [
+                        ('country', ['CA', 'US']),
+                        ('county', ['Village']),
+                        ('role', ['other role', 'role 1', 'role 2']),
+                    ],
+                },
+            ],
+        },
+    ]
+    assert result == {
+        'author': {'name': name, 'id': author.id, 'title': author.title},
+        'image': None,
+        'properties': expected,
+    }
+
+
 def test_author_properties_extracts_image():
     author = make_author(
         first_name='Some Other',
@@ -228,23 +333,66 @@ def test_author_properties_extracts_image():
     rank_image = baker.make(
         'PersonalAuthorPropertyRank', name='image', rank=30
     )
-    prop_image_1 = make_property(
+    prop_image_1 = baker.make(
+        'PersonalAuthorProperty',
         personal_author=author,
         name=rank_image.name,
         value='https://link-to-image-1.jpg',
+        qualifier='',
+        qualifier_value='',
     )
     assert prop_image_1.rank == 30
-    prop_image_2 = make_property(
+    prop_image_2 = baker.make(
+        'PersonalAuthorProperty',
         personal_author=author,
         name=rank_image.name,
         value='https://link-to-image-2.jpg',
+        qualifier='',
+        qualifier_value='',
     )
     assert prop_image_2.rank == 30
 
     result = DocumentPersonalAuthor.objects.properties(name)
 
-    # XXX: alt needs data definition, waiting on Paul for this
+    # Default alt if no 'media legend' qualifier is present
     image = {'url': prop_image_1.value, 'alt': f'Image of {name}'}
+    assert result == {
+        'author': {'name': name, 'id': author.id, 'title': author.title},
+        'image': image,
+        'properties': [],
+    }
+
+    # now define 'media legend' for the second image, it will not be used
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank_image.name,
+        value='https://link-to-image-2.jpg',
+        qualifier='media legend',
+        qualifier_value='The Legend for Image 2',
+    )
+
+    result = DocumentPersonalAuthor.objects.properties(name)
+    assert result == {
+        'author': {'name': name, 'id': author.id, 'title': author.title},
+        'image': image,
+        'properties': [],
+    }
+
+    # now define 'media legend' for the first image, it should be picked up
+    legend = 'The Legend for Image 1'
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank_image.name,
+        value='https://link-to-image-1.jpg',
+        qualifier='media legend',
+        qualifier_value=legend,
+    )
+
+    result = DocumentPersonalAuthor.objects.properties(name)
+
+    image['alt'] = legend
     assert result == {
         'author': {'name': name, 'id': author.id, 'title': author.title},
         'image': image,
@@ -264,15 +412,21 @@ def test_author_properties_groups_birth_data():
     rank_date_of_birth = baker.make(
         'PersonalAuthorPropertyRank', name='date of birth', rank=23
     )
-    prop_place_of_birth = make_property(
+    prop_place_of_birth = baker.make(
+        'PersonalAuthorProperty',
         personal_author=author,
         name=rank_place_of_birth.name,
         value='A city or location',
+        qualifier='',
+        qualifier_value='',
     )
-    prop_date_of_birth = make_property(
+    prop_date_of_birth = baker.make(
+        'PersonalAuthorProperty',
         personal_author=author,
         name=rank_date_of_birth.name,
         value='1979-01-01',
+        qualifier='',
+        qualifier_value='',
     )
 
     result = DocumentPersonalAuthor.objects.properties(name)
@@ -304,15 +458,21 @@ def test_author_properties_groups_death_data():
     rank_date_of_death = baker.make(
         'PersonalAuthorPropertyRank', name='date of death', rank=23
     )
-    prop_place_of_death = make_property(
+    prop_place_of_death = baker.make(
+        'PersonalAuthorProperty',
         personal_author=author,
         name=rank_place_of_death.name,
         value='A city or location',
+        qualifier='',
+        qualifier_value='',
     )
-    prop_date_of_death = make_property(
+    prop_date_of_death = baker.make(
+        'PersonalAuthorProperty',
         personal_author=author,
         name=rank_date_of_death.name,
         value='1979-01-01',
+        qualifier='',
+        qualifier_value='',
     )
 
     result = DocumentPersonalAuthor.objects.properties(name)
@@ -372,4 +532,189 @@ def test_author_properties_name_data_ignored():
         'author': {'name': name, 'id': author.id, 'title': author.title},
         'image': None,
         'properties': [],
+    }
+
+
+def test_author_properties_dates_and_qualifiers():
+    author = make_author()
+    name = author.full_name()
+    rank_place_of_birth = baker.make(
+        'PersonalAuthorPropertyRank', name='place of birth', rank=26
+    )
+    rank_date_of_birth = baker.make(
+        'PersonalAuthorPropertyRank', name='date of birth', rank=25
+    )
+    rank_place_of_death = baker.make(
+        'PersonalAuthorPropertyRank', name='place of death', rank=24
+    )
+    rank_date_of_death = baker.make(
+        'PersonalAuthorPropertyRank', name='date of death', rank=23
+    )
+
+    prop_place_of_birth = baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank_place_of_birth.name,
+        value='A city or location',
+        qualifier='',
+        qualifier_value='',
+    )
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank_place_of_birth.name,
+        value='A city or location',
+        qualifier='country',
+        qualifier_value='DE',
+    )
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank_place_of_birth.name,
+        value='A city or location',
+        qualifier='country',
+        qualifier_value='Austria-Hungary',
+    )
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank_place_of_birth.name,
+        value='A city or location',
+        qualifier='stress address',
+        qualifier_value='Some Address',
+    )
+
+    prop_date_of_birth = baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank_date_of_birth.name,
+        value='1979-01-01',
+        qualifier='',
+        qualifier_value='',
+    )
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank_date_of_birth.name,
+        value='1979-01-01',
+        qualifier='country',
+        qualifier_value='Bulgaria',
+    )
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank_date_of_birth.name,
+        value='1979-01-01',
+        qualifier='child of',
+        qualifier_value='Parent Name 1',
+    )
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank_date_of_birth.name,
+        value='1979-01-01',
+        qualifier='child of',
+        qualifier_value='Parent Name 2',
+    )
+
+    prop_place_of_death = baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank_place_of_death.name,
+        value='Another city',
+        qualifier='',
+        qualifier_value='',
+    )
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank_place_of_death.name,
+        value='Another city',
+        qualifier='country',
+        qualifier_value='Nazi Germany',
+    )
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank_place_of_death.name,
+        value='Another city',
+        qualifier='located in the administrative territorial entity',
+        qualifier_value='Berlin',
+    )
+
+    prop_date_of_death = baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank_date_of_death.name,
+        value='1999-01-01',
+        qualifier='',
+        qualifier_value='',
+    )
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank_date_of_death.name,
+        value='1999-01-01',
+        qualifier='cause',
+        qualifier_value='poisoning',
+    )
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank_date_of_death.name,
+        value='1999-01-01',
+        qualifier='something',
+        qualifier_value='one value',
+    )
+    baker.make(
+        'PersonalAuthorProperty',
+        personal_author=author,
+        name=rank_date_of_death.name,
+        value='1999-01-01',
+        qualifier='something',
+        qualifier_value='another value',
+    )
+
+    result = DocumentPersonalAuthor.objects.properties(name)
+
+    born = f'{prop_date_of_birth.value} ({prop_place_of_birth.value})'
+    died = f'{prop_date_of_death.value} ({prop_place_of_death.value})'
+    properties = [
+        {
+            'rank': 26,
+            'name': 'born',
+            'prop_values': [
+                {
+                    'value': born,
+                    'qualifiers': [
+                        ('child of', ['Parent Name 1', 'Parent Name 2']),
+                        ('country', ['Austria-Hungary', 'Bulgaria', 'DE']),
+                        ('stress address', ['Some Address']),
+                    ],
+                }
+            ],
+        },
+        {
+            'rank': 24,
+            'name': 'died',
+            'prop_values': [
+                {
+                    'value': died,
+                    'qualifiers': [
+                        ('cause', ['poisoning']),
+                        ('country', ['Nazi Germany']),
+                        (
+                            'located in the administrative territorial entity',
+                            ['Berlin'],
+                        ),
+                        ('something', ['another value', 'one value']),
+                    ],
+                }
+            ],
+        },
+    ]
+    assert result == {
+        'author': {'name': name, 'id': author.id, 'title': author.title},
+        'image': None,
+        'properties': properties,
     }
