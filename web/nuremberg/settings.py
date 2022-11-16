c79f753efe7a7c7e20c9c21f0f48721669d7d33e
Stephen D. Spencer <stephen@revsys.com>
2022-11-16 15:16:42 -0600

    abspath() doesn't seem to like being predictable in a container. BASE_DIR is set in the deployment Dockerfile but will revert to the 2-scoops road-to-frustration method if the var isn't set

--- web/nuremberg/settings.py
+++ web/nuremberg/settings.py
@@ -7,8 +7,8 @@ env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = env.str('BASE_DIR', os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 )
 
 
