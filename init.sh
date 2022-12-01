7beca96bea7fe57d69a82710cf0c9fab918a1a90
Stephen Spencer <stephen@revsys.com>
2022-12-01 16:54:04 -0600

    ...

--- init.sh
+++ init.sh
@@ -22,33 +22,34 @@ fi
 DOCKER_COMPOSE_EXEC="$DOCKER_COMPOSE exec -T"
 
 echo "Setting up sqlite"
-install -vd $HOME/tmp -m 0777
-unzip -p dumps/nuremberg_prod_dump_latest.sqlite3.zip > $HOME/tmp/nuremberg_dev.db
+install -vd $PWD/tmp -m 0777
+unzip -o -d $PWD/tmp dumps/nuremberg_prod_dump_latest.sqlite3.zip 
 
 $DOCKER_COMPOSE_EXEC ${web} cp -v /mnt/nuremberg_dev.db /tmp
 $DOCKER_COMPOSE_EXEC ${web} ./manage.py migrate || exit 1
 
-echo "Wait for Solr to be ready..."
-while ! $DOCKER_COMPOSE_EXEC ${solr} solr status >/dev/null 2>&1; do
-    sleep 1
-done
-echo "Solr ready!!!"
-
-echo "Setting up solr config"
-$DOCKER_COMPOSE cp solr_conf/ ${solr}:/opt/solr-8.11.2/solr_conf
-$DOCKER_COMPOSE_EXEC ${solr} cp -r /opt/solr-8.11.2/solr_conf $SOLR_HOME
-
-http_code=$( $DOCKER_COMPOSE_EXEC ${solr} curl -sS -o /dev/null -w '%{http_code}' "$SOLR_URL/admin/cores?action=reload&core=$SOLR_CORE" ) || exit 1
-
-if [[ $http_code == 200 ]]; then
-    echo "Solr core already exists"
-else
-    echo "Solr core does not exist, creating it"
-    $DOCKER_COMPOSE_EXEC ${solr} solr create_core -c $SOLR_CORE -d /opt/solr-8.11.2/solr_conf || exit 1
-fi
 
 if [[ -z ${SOLR_NO_RESTORE} ]];
 then
+	echo "Wait for Solr to be ready..."
+	while ! $DOCKER_COMPOSE_EXEC ${solr} solr status >/dev/null 2>&1; do
+	    sleep 1
+	done
+	echo "Solr ready!!!"
+
+	echo "Setting up solr config"
+
+	$DOCKER_COMPOSE cp solr_conf/ ${solr}:/opt/solr-8.11.2/solr_conf
+	$DOCKER_COMPOSE_EXEC ${solr} cp -r /opt/solr-8.11.2/solr_conf $SOLR_HOME
+
+	http_code=$( $DOCKER_COMPOSE_EXEC ${solr} curl -sS -o /dev/null -w '%{http_code}' "$SOLR_URL/admin/cores?action=reload&core=$SOLR_CORE" ) || exit 1
+
+	if [[ $http_code == 200 ]]; then
+	    echo "Solr core already exists"
+	else
+	    echo "Solr core does not exist, creating it"
+	    $DOCKER_COMPOSE_EXEC ${solr} solr create_core -c $SOLR_CORE -d /opt/solr-8.11.2/solr_conf || exit 1
+	fi
 	if [[ -n ${SOLR_RESTORE_SNAPSHOT} ]]; then
 	    echo "Restoring Solr snapshot $SOLR_SNAPSHOT_DIR"
 	    cat $SOLR_SNAPSHOT_DIR/* | $DOCKER_COMPOSE_EXEC ${solr} tar -xzv -f - --strip-component=1 -C $SOLR_HOME/data
