be10137fa19acbbb18cafb081cec4bdb634e7017
Stephen Spencer <stephen@revsys.com>
2022-12-04 13:44:56 -0600

    ftw

--- web/nuremberg/settings.py
+++ web/nuremberg/settings.py
@@ -7,8 +7,11 @@ env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = env.str('BASE_DIR', os.path.dirname(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = env.str(
+    'BASE_DIR',
+    os.path.dirname(
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    ),
 )
 
 LOCAL_DEVELOPMENT = env.bool("LOCAL_DEVELOPMENT", default=False)
@@ -186,7 +189,6 @@ LOGGING = {
 }
 
 
-
 ##############################################################################
 # Deployed Settings
 ##############################################################################
@@ -207,8 +209,8 @@ TRANSCRIPTS_BUCKET = env(
 
 if not LOCAL_DEVELOPMENT:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    #AWS_S3_ACCESS_KEY_ID = env('AWS_S3_ACCESS_KEY_ID')
-    #AWS_S3_SECRET_ACCESS_KEY = env('AWS_S3_SECRET_ACCESS_KEY')
+    # AWS_S3_ACCESS_KEY_ID = env('AWS_S3_ACCESS_KEY_ID')
+    # AWS_S3_SECRET_ACCESS_KEY = env('AWS_S3_SECRET_ACCESS_KEY')
     AWS_S3_REGION_NAME = 'sfo2'
     AWS_S3_ENDPOINT_URL = (
         f'https://{AWS_S3_REGION_NAME}.digitaloceanspaces.com'
