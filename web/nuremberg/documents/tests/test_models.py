7cf6332b89d9d29e3ec20e1be2d5302f225c81b9
Natalia <nataliabidart@gmail.com>
2022-12-05 10:05:32 -0300

    Show additional document metadata in details page

--- web/nuremberg/documents/tests/test_models.py
+++ web/nuremberg/documents/tests/test_models.py
@@ -17,26 +17,18 @@ from .helpers import make_author, make_document, make_document_text
 pytestmark = pytest.mark.django_db
 
 
-@pytest.mark.parametrize('day', [None, 0, 33, -1])
-@pytest.mark.parametrize('month', [None, 0, 13, -1])
-@pytest.mark.parametrize('year', [None, 0, -1])
-def test_document_date_invalid_date_params(day, month, year):
-    d = DocumentDate.objects.create(day=day, month=month, year=year)
-    assert d.as_date() is None
-
-
 @pytest.mark.parametrize('year, month, day', [(2022, 2, 29), (2022, 4, 31)])
 def test_document_date_invalid_date(day, month, year):
     d = DocumentDate.objects.create(day=day, month=month, year=year)
     assert d.as_date() is None
 
 
-@pytest.mark.parametrize('day', [1, 30])
-@pytest.mark.parametrize('month', [1, 12])
-@pytest.mark.parametrize('year', [1, 1000, 2050])
-def test_document_date_valid_as_date(day, month, year):
-    d = DocumentDate.objects.create(day=day, month=month, year=year)
-    assert d.as_date() == datetime.date(year, month, day)
+def test_document_date_valid_as_date():
+    today = datetime.date.today()
+    d = DocumentDate.objects.create(
+        day=today.day, month=today.month, year=today.year
+    )
+    assert d.as_date() == today
 
 
 def test_document_total_pages():
