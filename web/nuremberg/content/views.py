c3a49fc8a6457ec2347953cd044d0811323320d6
nessita <nataliabidart@gmail.com>
2023-01-18 17:34:20 -0300

    Add new section to landing page: project news (#148)
    The 3 sections Approaches, Trial Transcripts, and Project Progress should be
    replaced by the 2 new sections The Trials and Nuremberg Project News and
    Project Analyst's Reports.
    
    The reports are taken from the dedicated model in the DB and there is also a
    new report detail view provided.

--- web/nuremberg/content/views.py
+++ web/nuremberg/content/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
 
 from nuremberg.content.models import AnalystReport
 
@@ -16,3 +17,18 @@ class LandingView(ContentView):
 
     template_name = 'content/landing.html'
     context = {'query': '', 'reports': AnalystReport.objects.all()}
+
+
+class ReportDetailView(DetailView):
+
+    model = AnalystReport
+    template_name = 'content/report.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        for pos in ('next', 'previous'):
+            try:
+                context[pos] = getattr(self.object, f'get_{pos}_by_date')()
+            except AnalystReport.DoesNotExist:
+                context[pos] = None
+        return context
