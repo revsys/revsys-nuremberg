e769cc6997ec8c9b050cb390b0f584ea2187505a
Natalia <nataliabidart@gmail.com>
2023-01-18 10:22:41 -0300

    Updated DB dump with updated analyst reports and trial info

--- web/nuremberg/content/models.py
+++ web/nuremberg/content/models.py
@@ -4,10 +4,21 @@ from django.db import models
 class AnalystReport(models.Model):
     id = models.AutoField(db_column='RecordID', primary_key=True)
     analyst = models.CharField(db_column='Analyst', max_length=100)
-    date = models.CharField(db_column='Date', max_length=100)
+    date = models.DateField(db_column='DateSort')
+    date_display = models.CharField(db_column='DateDisplay', max_length=100)
     report = models.CharField(db_column='Report', max_length=15000)
-    updated_at = models.DateTimeField(db_column='RecordUpdated')
 
     class Meta:
         managed = False
         db_table = 'tblDocAnalystReports'
+
+
+class TrialInfo(models.Model):
+    id = models.AutoField(db_column='RecordID', primary_key=True)
+    name = models.CharField(db_column='TrialName', max_length=200)
+    alias = models.CharField(db_column='TrialNameAlias', max_length=200)
+    description = models.TextField(db_column='Description')
+
+    class Meta:
+        managed = False
+        db_table = 'tblTrialsInfo'
