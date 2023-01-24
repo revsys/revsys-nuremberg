9c314fc422756e6b1178011b85c467200d6db817
nessita <nataliabidart@gmail.com>
2023-01-24 11:27:10 -0300

    New dump with newer `tblDocAnalystReports` including newlines in reports (#161)

--- web/nuremberg/content/models.py
+++ web/nuremberg/content/models.py
@@ -8,7 +8,7 @@ class AnalystReport(models.Model):
     id = models.AutoField(db_column='RecordID', primary_key=True)
     analyst = models.CharField(db_column='Analyst', max_length=100)
     date = models.DateField(db_column='DateSort')
-    date_display = models.CharField(db_column='DateDisplay', max_length=100)
+    date_display = models.CharField(db_column='Date', max_length=100)
     report = models.CharField(db_column='Report', max_length=15000)
 
     class Meta:
