d964cc9b8d40ba9bca5da44cc2d9148d8c3c3743
nessita <nataliabidart@gmail.com>
2023-02-24 18:02:32 -0300

    Ensure document text highlight handles exact phrases (#200)
    This branch extends the custom Highlighter so search terms for exact phrase
    match (ie using double quotes) is acknowledged and respected when doing the
    actual highlighting.

--- web/nuremberg/documents/views.py
+++ web/nuremberg/documents/views.py
@@ -1,3 +1,5 @@
+import re
+
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import View
@@ -6,7 +8,30 @@ from haystack.utils import Highlighter
 from .models import Document, DocumentPersonalAuthor, DocumentText
 
 
+EXACT_PHRASE_RE = re.compile(r'"([^"]*)"|([^"]*)')
+
+
 class DocumentHighlighter(Highlighter):
+    def __init__(self, query, **kwargs):
+        super().__init__(query, **kwargs)
+
+        # Extract all exact phrases and treat them as "a single word"
+        matches = EXACT_PHRASE_RE.findall(query)
+        exact_phrases, non_exact = list(zip(*matches))
+        self.query_words = {
+            phrase.strip().lower()
+            for phrase in exact_phrases
+            if phrase.strip()
+        }
+        self.query_words.update(
+            {
+                word.strip().lower()
+                for item in non_exact
+                for word in item.split()
+                if item and not word.startswith("-")
+            }
+        )
+
     def find_window(self, highlight_locations):
         # Do not truncate the text at all -- show everything, from start to end
         return (0, len(self.text_block))
