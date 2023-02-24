45d6a5d0d9844e7a57d106863c236cdf441c0725
Frank Wiles <frank@revsys.com>
2023-02-24 13:48:14 -0600

    Author Flyout z-index issue
    - Wasn't z-index'ed high enough for the Document page

--- web/nuremberg/settings.py
+++ web/nuremberg/settings.py
@@ -7,10 +7,12 @@ env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(env.str(
-    'BASE_DIR',
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-))
+BASE_DIR = Path(
+    env.str(
+        'BASE_DIR',
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+    )
+)
 
 LOCAL_DEVELOPMENT = env.bool("LOCAL_DEVELOPMENT", default=False)
 
@@ -235,7 +237,7 @@ DJANGO_VITE_ASSETS_PATH = BASE_DIR.joinpath("frontend/dist/").as_posix()
 if LOCAL_DEVELOPMENT:
     SECRET_KEY = 'supersecret'
     DEBUG = True
-    COMPRESS_ENABLED = False
+    COMPRESS_ENABLED = True
 
     CACHES = {
         'default': {'BACKEND': 'django.core.cache.backends.dummy.DummyCache'}
