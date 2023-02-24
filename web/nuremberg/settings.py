bc73a53a3fc80928cb5ab652fd97c7d27a9e810a
Natalia <nataliabidart@gmail.com>
2023-02-24 17:34:59 -0300

    Ensure document text highlight handles exact phrases
    This branch extends the custom Highlighter so search terms for exact phrase
    match (ie using double quotes) is acknowledged and respected when doing the
    actual highlighting.

--- web/nuremberg/settings.py
+++ web/nuremberg/settings.py
@@ -7,10 +7,12 @@ env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(env.str(
-    'BASE_DIR',
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-))
+BASE_DIR = Path(
+    env.str(
+        'BASE_DIR',
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+    )
+)
 
 LOCAL_DEVELOPMENT = env.bool("LOCAL_DEVELOPMENT", default=False)
 
