dc210c9aae0bd466a8e071cfb67735e742ee1d34
Frank Wiles <frank@revsys.com>
2023-03-05 11:25:55 -0600

    Implement clear form

--- web/frontend/src/views/Search.jsx
+++ web/frontend/src/views/Search.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react'
 import { useSearchParams } from "react-router-dom"
-
 import SearchBar from './SearchBar'
 
 const Search = (props) => {
-  // const params = new URLSearchParams(window.location.search)
   const [params, setParams] = useSearchParams()
 
   return (
     <section role="search" aria-label="Search the archive" className="theme-beige thin">
       <h1 className="h4">Search the archive</h1>
       <div className="full-width">
-        <SearchBar query={params.get('q')} />
+        <SearchBar
+          query={params.get('q')}
+          setParams={setParams}
+        />
         <div className="show-advanced-search"><a>Advanced Search</a></div>
         {/* <p>{% include 'search/advanced-search-form.html' with form=advanced_search_form %}</p> */}
       </div>
