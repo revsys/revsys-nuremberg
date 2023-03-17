f9d6305e85b0787109c881ed1ad5471073c662a3
Natalia <nataliabidart@gmail.com>
2023-03-17 14:53:38 -0300

    Preparation work for switching Transcript highlighting from JS to pure Python

--- web/nuremberg/documents/views.py
+++ web/nuremberg/documents/views.py
@@ -1,48 +1,16 @@
-import re
-
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import View
-from haystack.utils import Highlighter
 
+from nuremberg.core.highlighter import NurembergHighlighter
 from .models import Document, DocumentPersonalAuthor, DocumentText
-
-
-EXACT_PHRASE_RE = re.compile(r'"([^"]*)"|([^"]*)')
-
-
-class DocumentHighlighter(Highlighter):
-    def __init__(self, query, **kwargs):
-        super().__init__(query, **kwargs)
-
-        # Extract all exact phrases and treat them as "a single word"
-        matches = EXACT_PHRASE_RE.findall(query)
-        exact_phrases, non_exact = list(zip(*matches))
-        self.query_words = {
-            phrase.strip().lower()
-            for phrase in exact_phrases
-            if phrase.strip()
-        }
-        self.query_words.update(
-            {
-                word.strip().lower()
-                for item in non_exact
-                for word in item.split()
-                if item and not word.startswith("-")
-            }
-        )
-
-    def find_window(self, highlight_locations):
-        # Do not truncate the text at all -- show everything, from start to end
-        return (0, len(self.text_block))
 
 
 class Show(View):
     template_name = 'documents/show.html'
 
     def highlight_query(self, text, query):
-        highlight = DocumentHighlighter(query, html_tag='mark')
-        return highlight.highlight(text)
+        return NurembergHighlighter(query).highlight(text)
 
     def get(self, request, document_id, *args, **kwargs):
         mode = request.GET.get('mode', 'image')
