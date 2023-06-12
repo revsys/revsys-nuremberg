c0a3acb99fba7fc72bf90c31c6519a32dc2a4dc1
Frank Wiles <frank@revsys.com>
2023-06-12 14:09:02 -0500

    Search date failure (#262)
    * Verify search date failure fixed
    
    - Fix author hover issue on subsequent searches
    - Move old search to `/search/old-search`
    - Move new search into place
    
    * Rebuild ViteJS

--- web/nuremberg/search/urls.py
+++ web/nuremberg/search/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 app_name = 'search'
 urlpatterns = [
+    path('', views.NewSearch.as_view(), name='search'),
     path('', views.Search.as_view(), name='search'),
     path('advanced', views.advanced_search, name='advanced-search'),
     path(
@@ -12,5 +13,6 @@ urlpatterns = [
         TemplateView.as_view(template_name='search/advanced-search-help.html'),
         name='search-help',
     ),
+    path('old-search', views.NewSearch.as_view(), name='old-search'),
     path('new-search', views.NewSearch.as_view(), name='new-search'),
 ]
