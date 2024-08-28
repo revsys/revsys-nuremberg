e88babab67d9677ce4e88475bcc7c48fea2c29b0
Frank Wiles <frank@revsys.com>
2024-08-27 21:27:14 -0500

    Turn on offline django-compressor
    We need to generate a consistent hash because django-compressor is
    stupid.

--- web/nuremberg/settings.py
+++ web/nuremberg/settings.py
@@ -147,6 +147,8 @@ COMPRESS_PRECOMPILERS = (('text/less', 'lessc {infile} {outfile}'),)
 
 COMPRESS_STORAGE = 'compressor.storage.GzipCompressorFileStorage'
 
+COMPRESS_OFFLINE = True
+
 # whitenoise settings
 # https://warehouse.python.org/project/whitenoise/
 
@@ -244,6 +246,7 @@ if LOCAL_DEVELOPMENT:
     SECRET_KEY = 'supersecret'
     DEBUG = True
     COMPRESS_ENABLED = True
+    COMPRESS_OFFLINE = False
 
     CACHES = {
         'default': {'BACKEND': 'django.core.cache.backends.dummy.DummyCache'}
