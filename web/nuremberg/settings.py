f0883beb7797e4d61d537305190ff3c552ffbf23
Stephen D. Spencer <stephen@revsys.com>
2025-06-05 14:53:33 -0500

    put admin and sessions back for the moment

--- web/nuremberg/settings.py
+++ web/nuremberg/settings.py
@@ -31,8 +31,10 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
